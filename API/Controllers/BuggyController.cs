7881eba312b22d50e20d8f83a8200f0edc596974
momen khaled <momenkhaled360@gmail.com>
2026-03-06 19:49:26 +0200

    Implemented global API error handling with middleware and tested errors via a controller. Handled errors in Angular using an HTTP interceptor and added custom 404 and 500 error pages.

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController :BaseApiController
+    {
+        [HttpGet("auth")]
+        public IActionResult GetAuth()
+        {
+            return Unauthorized(); // 401
+        }
+
+        [HttpGet("not-found")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("server-error")]
+        public IActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");
+        }
+
+        [HttpGet("bad-request")]
+        public IActionResult GetBadRequest() // 400
+        {
+            return BadRequest("This was not a good request");
+            //return BadRequest();
+        }
+
+    }
+}
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,12 +5,12 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
-        public required string DisplayName { get; set; }
+        public string DisplayName { get; set; } = "";
         [Required]
         [EmailAddress]
-        public required string Email { get; set; }
+        public  string Email { get; set; } = "";
         [Required]
         [MinLength(4)]
-        public required string Password { get; set; } 
+        public  string Password { get; set; } = "";
     }
 }
